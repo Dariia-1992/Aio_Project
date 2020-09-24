629d2cbddcefde9f46ceab11e0cd03270cb2db34
Dariia <pers4040@gmail.com>
2020-09-24 17:19:42 +0300

    fix

--- app/src/main/java/com/example/aio_project/fragment/MainFragment.java
+++ app/src/main/java/com/example/aio_project/fragment/MainFragment.java
@@ -1,6 +1,24 @@
 package com.example.aio_project.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.aio_project.R;
+
 public class MainFragment extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
 }
