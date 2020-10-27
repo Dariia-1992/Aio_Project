85f99fdea20e2d68a1244f7a3a8029698a541987
Alex Matrosov <Alexey.S.Matrosov@gmail.com>
2020-10-27 05:26:09 +0200

    Add toolbar to TabsFragment

--- app/src/main/java/com/example/aio_project/fragment/TabsFragment.java
+++ app/src/main/java/com/example/aio_project/fragment/TabsFragment.java
@@ -1,5 +1,12 @@
 package com.example.aio_project.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aio_project.R;
+
 import androidx.fragment.app.Fragment;
 
 /**
@@ -7,4 +14,12 @@ import androidx.fragment.app.Fragment;
  */
 
 public class TabsFragment extends Fragment {
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_tabs, container, false);
+
+        return view;
+    }
 }
