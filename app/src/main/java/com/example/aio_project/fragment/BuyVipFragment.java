bd865b1dc30832c11e2b5249277928a80cb2f351
Alex Matrosov <Alexey.S.Matrosov@gmail.com>
2020-10-27 04:33:58 +0200

    Add BuyVip fragment to navigation

---
+++ app/src/main/java/com/example/aio_project/fragment/BuyVipFragment.java
@@ -0,0 +1,10 @@
+package com.example.aio_project.fragment;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by Alexey Matrosov on 27.10.2020.
+ */
+
+public class BuyVipFragment extends Fragment {
+}
