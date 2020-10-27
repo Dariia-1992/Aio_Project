3e68afd4d46b2cbc26e01d7c0e1bd63f529d48d8
Alex Matrosov <Alexey.S.Matrosov@gmail.com>
2020-10-27 22:09:25 +0200

    refactor ModelDTO

--- app/src/main/java/com/example/aio_project/model/ModelDTO.java
+++ app/src/main/java/com/example/aio_project/model/ModelDTO.java
@@ -2,56 +2,34 @@ package com.example.aio_project.model;
 
 public class ModelDTO {
 
-    private String modeTypeId;
+    // Common
     private String title;
+    private String categoryid;
     private String downloadcount;
-    private int rating;
-    //private  timestamp;
     private String viewcount;
+    private String rating;
 
+    // Maps, Mods, Seeds, Textures
     private String description;
+
+    // Maps, Mods, Textures
     private String fileurl;
     private String isupload;
 
+    // Seeds
     private String seed;
 
-    public String getModeTypeId() {
-        return modeTypeId;
-    }
+    // region Getters
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title; }
+    public String getCategoryId() { return categoryid; }
+    public String getDownloadsCount() { return downloadcount; }
+    public String getViewsCount() { return viewcount; }
+    public String getRating() { return rating; }
+    public String getDescription() { return description; }
+    public String getFileUrl() { return fileurl; }
+    public String getIsUpload() { return isupload; }
+    public String getSeed() { return seed; }
 
-    public String getDownloadcount() {
-        return downloadcount;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-/*    public String getTimestamp() {
-        return timestamp;
-    }*/
-
-    public String getViewcount() {
-        return viewcount;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getFileurl() {
-        return fileurl;
-    }
-
-    public String isIsupload() {
-        return isupload;
-    }
-
-    public String getSeed() {
-        return seed;
-    }
+    // endregion
 }
